42a6a548e521030ad688757cc073cc9df9207e33
Peter McWilliams <petemcw@gmail.com>
2013-03-20 12:24:10 -0500

    Updates module to work with Flat Products index

--- README.md
+++ README.md
@@ -13,6 +13,21 @@ Installation
 1. Clone module with [modman](https://github.com/colinmollenhour/modman)
 2. Delete all contents of the Magento cache
 
+## Example:
+
+* For this module to do its job, you must specify the following layout update on 
+  a category via the administration interface:
+
+<reference name="category.products">
+    <block type="ash_onsale/product_list" name="product_list" template="catalog/product/list.phtml">
+        <block type="catalog/product_list_toolbar" name="product_list_toolbar" template="catalog/product/list/toolbar.phtml">
+            <block type="page/html_pager" name="product_list_toolbar_pager"/>
+        </block>
+        <action method="setColumnCount"><columns>4</columns></action>
+        <action method="setToolbarBlockName"><name>product_list_toolbar</name></action>
+    </block>
+</reference>
+
 ```
 @copyright  Copyright (c) 2013 August Ash, Inc. (http://www.augustash.com)
 ```
--- code/Block/Product/List.php
+++ code/Block/Product/List.php
@@ -6,6 +6,7 @@
  * @package     Ash_Onsale
  * @copyright   Copyright (c) 2013 August Ash, Inc. (http://www.augustash.com)
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author      August Ash Team <core@augustash.com>
  */
 
 /**
@@ -24,56 +25,30 @@ class Ash_Onsale_Block_Product_List extends Mage_Catalog_Block_Product_List
     protected function _getProductCollection()
     {
         $collection = parent::_getProductCollection();
-        $collection->getSelect()
-            ->where(new Zend_Db_Expr('price_index.final_price < price_index.price'));
 
-        return $collection;
-    }
-
-    /**
-     * Retrieve loaded category collection
-     *
-     * @return Mage_Eav_Model_Entity_Collection_Abstract
-     */
-    protected function _getProductCollection()
-    {
-        if (is_null($this->_productCollection)) {
-            $layer = $this->getLayer();
-            /* @var $layer Mage_Catalog_Model_Layer */
-            if ($this->getShowRootCategory()) {
-                $this->setCategoryId(Mage::app()->getStore()->getRootCategoryId());
-            }
-
-            // if this is a product view page
-            if (Mage::registry('product')) {
-                // get collection of categories this product is associated with
-                $categories = Mage::registry('product')->getCategoryCollection()
-                    ->setPage(1, 1)
-                    ->load();
-                // if the product is associated with any category
-                if ($categories->count()) {
-                    // show products from this category
-                    $this->setCategoryId(current($categories->getIterator()));
-                }
-            }
-
-            $origCategory = null;
-            if ($this->getCategoryId()) {
-                $category = Mage::getModel('catalog/category')->load($this->getCategoryId());
-                if ($category->getId()) {
-                    $origCategory = $layer->getCurrentCategory();
-                    $layer->setCurrentCategory($category);
-                }
-            }
-            $this->_productCollection = $layer->getProductCollection();
-
-            $this->prepareSortableFieldsByCategory($layer->getCurrentCategory());
-
-            if ($origCategory) {
-                $layer->setCurrentCategory($origCategory);
-            }
+        /*
+         * Collections using Product Flat index require different processing.
+         */
+        if (Mage::getStoreConfigFlag('catalog/frontend/flat_catalog_product')) {
+            $collection
+                ->getSelect()
+                ->where(new Zend_Db_Expr('price_index.final_price < price_index.price'));
+        } else {
+            $today = date('Y-m-d', time());
+            $collection
+                ->addAttributeToFilter('special_price', array('is' => new Zend_Db_Expr('not null')))
+                ->addAttributeToFilter('special_from_date', array(
+                    'or' => array(
+                        0 => array('date' => true, 'to' => $today),
+                        1 => array('is' => new Zend_Db_Expr('null')),
+                    )), 'left')
+                ->addAttributeToFilter('special_to_date', array(
+                    'or' => array(
+                        0 => array('date' => true, 'from' => $today),
+                        1 => array('is' => new Zend_Db_Expr('null')),
+                    )), 'left');
         }
 
-        return $this->_productCollection;
+        return $collection;
     }
 }
